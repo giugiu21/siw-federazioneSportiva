d0bc25f8fc5b2fcb41099dc8611d1bffa7ca85ed
giugiu21 <giulia.alemanno.ga@gmail.com>
2024-02-13 10:36:17 +0100

    modifiche varie

--- src/main/java/it/uniroma3/siw/repository/TeamRepository.java
+++ src/main/java/it/uniroma3/siw/repository/TeamRepository.java
@@ -3,9 +3,7 @@ package it.uniroma3.siw.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import it.uniroma3.siw.model.President;
@@ -17,12 +15,12 @@ public interface TeamRepository extends CrudRepository<Team, Long>{
 	public boolean existsByName(String name);
 	
 	public void deleteByName(String name);
-	
-//	@Query(value = "SELECT t FROM Teams t WHERE t.president = :president",  nativeQuery=true)
-//	public Team getTeamFromPresident(@Param("president") President president);
-	
+
 	
 	public Team findByPresident(President president);
 
 	public List<Team> findByPresidentIsNotNull();
+	
+	
+	
 }
